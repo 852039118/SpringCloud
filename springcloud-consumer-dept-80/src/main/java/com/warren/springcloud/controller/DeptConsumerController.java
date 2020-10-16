7bea4e49149e414e359859814ec3f47dab8ca02a
wkj <852039118@qq.com>
2020-10-16 18:28:31 +0800

    provider consumer

--- springcloud-consumer-dept-80/src/main/java/com/warren/springcloud/controller/DeptConsumerController.java
+++ springcloud-consumer-dept-80/src/main/java/com/warren/springcloud/controller/DeptConsumerController.java
@@ -1,11 +1,38 @@
 package com.warren.springcloud.controller;
 
+import com.warren.springcloud.pojo.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class DeptConsumerController {
 
     // 消费者，不应该有Service层
-    // RestTemplate...供我们直接调用就可以了！注册到Spring中
+    // RestTemplate...供我们直接调用就可以了！注册到Spring中（在config中配置）
+    // (URL, class<T> responseType, 实体:Map)
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+
+    @RequestMapping("/dept/add")
+    public boolean add(@RequestBody Dept dept){
+        return restTemplate.postForObject(REST_URL_PREFIX+"/dept/add",dept,Boolean.class);
+    }
+
+    @RequestMapping("/dept/get/{id}")
+    public Dept get(@PathVariable("id") long id){
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/"+id,Dept.class);
+    }
+
+    @RequestMapping("/dept/list")
+    public List<Dept> queryAll(){
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list",List.class);
+    }
 }
--- springcloud-provider-dept-8001/src/main/java/com/warren/springcloud/controller/DeptController.java
+++ springcloud-provider-dept-8001/src/main/java/com/warren/springcloud/controller/DeptController.java
@@ -4,10 +4,7 @@ import com.warren.springcloud.pojo.Dept;
 import com.warren.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +16,7 @@ public class DeptController {
     private DeptService deptService;
 
     @PostMapping("/dept/add")
-    public boolean addDept(Dept dept){
+    public boolean addDept(@RequestBody Dept dept){
         return deptService.addDept(dept);
     }
 
